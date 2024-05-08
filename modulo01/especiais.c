33c42ea54cf8172db0c7e4ac7b0d25ebb2a6e980
JeiversonChristian <jeiversonc@gmail.com>
2024-05-08 13:25:55 -0300

    testando caracteres especiais no printf

---
+++ modulo01/especiais.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    printf("testando comandos\n");
+    printf("pulando linha com \\n\n");
+    printf("l1\n");
+    printf("l2\n");
+    printf("tabulando com \\t\n");
+    printf("\ttab\n");
+    printf("voltando um espaco com \\b\n");
+    printf("a \bb\n");
+    printf("voltando para o inicio da linha com \\r\n");
+    printf("vai ter um a antes dessa frase \ra\n");
+    printf("usando aspas com \\\"\n");
+    printf("\"\"\n");
+    printf("sinal de ?\n");
+    printf("beep com \\a ou \\7\n");
+    printf("\a\n");
+    printf("sinal de %%%%\n");
+    printf("%%\n");
+    printf("\%\n");
+    system("pause");
+}
