4fcaacc53068163e9b1a19750b058e06b94021c9
JeiversonChristian <jeiversonc@gmail.com>
2024-05-21 10:18:17 -0300

    aprendendo a imprimir int

---
+++ modulo01/saidas_formatadas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    // para imprimir um número inteiro usa-se %d ou %i
+    printf("%d",400);
+    printf("\n");
+    printf("%i",400);
+    printf("\n");
+    system("pause");
+}
