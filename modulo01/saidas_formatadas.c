fe19c0040ab9f19a0f0921dfda3123690439af3b
JeiversonChristian <jeiversonc@gmail.com>
2024-05-21 11:41:06 -0300

    printando floats

--- modulo01/saidas_formatadas.c
+++ modulo01/saidas_formatadas.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 void main(){
+    setlocale(LC_ALL, "Portuguese");
+
     // para imprimir um número inteiro usa-se %d ou %i
     printf("%d",400);
     printf("\n");
@@ -14,6 +17,12 @@ void main(){
 
     // misturando os dois tipos
     printf("%s tem %i anos de idade.", "Jeiverson", 31);
+    printf("\n");
+
+    // floats
+    printf("Seu atual peso é de %f kg", 94.5);
+    printf("\n");
+    printf("Digo, é de %.2f kg", 94.5);
     printf("\n");
 
     system("pause");
